6a25e861eed693485d66582497e17375d63afb71
kishudilantha <113208110+inblognet@users.noreply.github.com>
2022-09-20 19:58:00 -0700

    Add files via upload

---
+++ custom.js
@@ -0,0 +1,22 @@
+window.addEventListener("load",function(){
+    setTimeout(
+        function open(event){
+            document.querySelector(".popup").style
+            .display ="block";
+            
+
+        },
+        1000
+    )
+})
+
+
+
+document.querySelector("#close").addEventListener
+("click", function(){
+    document.querySelector(".popup").style.display ="none";
+
+});
+
+
+
