1e6ab63cbb0a870e37d2e12d258c314012c116fb
kishudilantha <113208110+inblognet@users.noreply.github.com>
2022-10-01 22:25:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,346 @@
+let products = {
+  data: [
+    {
+      productName: "Audi 2022",
+      category: "catgory1",
+      price: "21,252,800",
+      image: "Audi_SQ5_Sportback_3_0T_Prestige_quattro_2023.jpg",
+    },
+    {
+      productName: "Audi 2022",
+      category: "catgory1",
+      price: "13,310,067",
+      image: "Audi_Q3_2023.jpg",
+    },
+
+    {
+      productName: "BMW X1",
+      category: "catgory2",
+      price: "14,478,355",
+      image: "BMW_X1_2023_1.jpg",
+    },
+    {
+      productName: "Land rover 2021",
+      category: "catgory4",
+      price: "38,393,694",
+      image: "Land_Rover_Range_Rover_Westminster_2021.jpg",
+    },
+
+
+    {
+      productName: "BMW 7 Series",
+      category: "catgory4",
+      price: "35,066,909",
+      image: "BMW-7-Series-745e-xDrive-Plug-In-Hybrid-2021.jpg",
+    },
+
+    {
+      productName: "Jeep wagoneer 2022",
+      category: "catgory4",
+      price: "32,412,208",
+      image: "Jeep_Wagoneer_Series_III_2022.jpg",
+    },
+
+
+    {
+      productName: "audi TTRS 25 quattro",
+      category: "catgory1",
+      price: "29,613,910",
+      image: "Audi_TT_RS_2_5_TFSI_quattro_Heritage_Edition_2022_1.jpg",
+    },
+    {
+      productName: "Honda hybrid 2023",
+      category: "catgory3",
+      price: "None",
+      image: "Honda_Accord_Hybrid_Touring_2023.jpg",
+    },
+    {
+      productName: "BMW M3",
+      category: "catgory2",
+      price: "64,904,863",
+      image: "BMW_M3_Touring_2023.jpg",
+    },
+    {
+      productName: "Honda pilot 2022",
+      category: "catgory3",
+      price: "14,282,726",
+      image: "Honda_Pilot_2022.jpg",
+    },
+
+    {
+      productName: "Honda AWD 2022",
+      category: "catgory3",
+      price: "15,977,566",
+      image: "Honda_Passport_Sport_AWD_2022.jpg",
+    },
+
+    {
+      productName: "Dodge Hornet GT 2023",
+      category: "catgory2",
+      price: "12,561,410",
+      image: "Dodge_Hornet_GT_Plus_2023.jpg",
+    },
+    {
+      productName: "Enyaq iV 2022",
+      category: "catgory2",
+      price: "14,999,020",
+      image: "Karma_Enyaq_iV_50_2022.jpg",
+    },
+
+    {
+      productName: "Ford Escape SE 2022",
+      category: "catgory2",
+      price: "9,996,355",
+      image: "Ford_Escape_SE_2022.jpg",
+    },
+
+
+    {
+      productName: "Renault Triber 2019",
+      category: "catgory2",
+      price: "2,979,700",
+      image: "Renault_Triber_RXT_2019.jpg",
+    },
+
+
+    {
+      productName: "Suzuki Celerio 2023",
+      category: "catgory2",
+      price: "none",
+      image: "Suzuki_Celerio_2023.jpg",
+    },
+
+    {
+      productName: "Suzuki Alto K10 2022",
+      category: "catgory2",
+      price: "2,513,000",
+      image: "Suzuki_Alto_K10_VXi_AT_2022.jpg",
+    },
+
+    {
+      productName: "Suzuki Dzire VXI CNG 2022",
+      category: "catgory2",
+      price: "3,697,700",
+      image: "Suzuki_Dzire_VXI_CNG_2022.jpg",
+    },
+
+    {
+      productName: "Suzuki S Cross Zeta 2022",
+      category: "catgory2",
+      price: "4,918,300",
+      image: "Suzuki_S_Cross_Zeta_2022.jpg",
+    },
+
+
+    {
+      productName: "Suzuki Brezza 2022",
+      category: "catgory2",
+      price: "5,654,250",
+      image: "Suzuki_Brezza_ZXI_A_DT_2022.jpg",
+    },
+    {
+      productName: "Suzuki XL6 Alpha AT 2023",
+      category: "catgory2",
+      price: "6,677,400",
+      image: "Suzuki_XL6_Alpha_AT_2023.jpg",
+    },
+    {
+      productName: "Jeep Gladiator Mojave 2023",
+      category: "catgory2",
+      price: "17,705,880",
+      image: "Jeep_Gladiator_Mojave_2023.jpg",
+    },
+
+    {
+      productName: "Jeep Grand Cherokee Summit Reverse 2022",
+      category: "catgory2",
+      price: "21,647,700",
+      image: "Jeep_Grand_Cherokee_Summit_Reverse_2022.jpg",
+    },
+    {
+      productName: "Jeep Wrangler Unlimited 2022",
+      category: "catgory2",
+      price: "12,058,810",
+      image: "Jeep_Wrangler_Unlimited_High_Tide_2022.jpg",
+    },
+    
+    {
+      productName: "Jeep Wrangler Hybrid 2022",
+      category: "catgory2",
+      price: "18,495,680",
+      image: "Jeep_Wrangler_Sahara_4xe_Plug_In_Hybrid_2022.jpg",
+    },
+
+    {
+      productName: "Toyota GR Yaris 2021",
+      category: "catgory2",
+      price: "avalable soon",
+      image: "Toyota-GR_Yaris-2021.jpg",
+    },
+
+
+
+    {
+      productName: "Toyota Corolla Hatchback SE 2022",
+      category: "catgory2",
+      price: "7,951,850",
+      image: "Toyota_Corolla_Hatchback_SE_2022.jpg",
+    },
+
+    {
+      productName: "Toyota Corolla Hatchback SE Nightshade Edition 2023",
+      category: "catgory2",
+      price: "8,188,790",
+      image: "Toyota_Corolla_Hatchback_SE_Nightshade_Edition_2023.jpg",
+    },
+
+
+    {
+      productName: "Toyota Tundra 2023",
+      category: "catgory2",
+      price: "13,642,000",
+      image: "Toyota_Tundra_2023.jpg",
+    },
+
+
+    {
+      productName: "Toyota Glanza G 2022 ",
+      category: "catgory2",
+      price: "3,880,790",
+      image: "Toyota_Glanza_G_2022.jpg",
+    },
+
+
+
+    {
+      productName: "Toyota RAV4 Prime 2023",
+      category: "catgory2",
+      price: "14,826,700",
+      image: "Toyota_RAV4_Prime_2023.jpg",
+    },
+
+    {
+      productName: "Mazda 3 Sedan Premium 2022",
+      category: "catgory2",
+      price: "9,818,650",
+      image: "Mazda_3_Sedan_Premium_2022.jpg",
+    },
+
+
+    {
+      productName: "Mazda 3 Sedan Select 2022",
+      category: "catgory2",
+      price: "8,292,900",
+      image: "Mazda_3_Sedan_Select_2022.jpg",
+    },
+
+    {
+      productName: "Mazda CX 50 2023",
+      category: "catgory2",
+      price: "9,621,200",
+      image: "Mazda_CX_50_2023_1.jpg",
+    },
+
+    {
+      productName: "Mazda CX 30 25S Preferred Package 2022",
+      category: "catgory2",
+      price: "9,926,350",
+      image: "Mazda_CX_30_2_5_S_Preferred_Package_2022.jpg",
+    },
+
+    {
+      productName: "Mazda CX 50 25 Turbo 2023",
+      category: "catgory2",
+      price: "13,067,600",
+      image: "Mazda_CX_50_2_5_Turbo_2023.jpg",
+    },
+
+
+
+
+  ],
+};
+
+for (let i of products.data) {
+  //Create Card
+  let card = document.createElement("div");
+  //Card should have category and should stay hidden initially
+  card.classList.add("card", i.category, "hide");
+  //image div
+  let imgContainer = document.createElement("div");
+  imgContainer.classList.add("image-container");
+  //img tag
+  let image = document.createElement("img");
+  image.setAttribute("src", i.image);
+  imgContainer.appendChild(image);
+  card.appendChild(imgContainer);
+  //container
+  let container = document.createElement("div");
+  container.classList.add("container");
+  //product name
+  let name = document.createElement("h5");
+  name.classList.add("product-name");
+  name.innerText = i.productName.toUpperCase();
+  container.appendChild(name);
+  //price
+  let price = document.createElement("h6");
+  price.innerText = "LKR:" + i.price;
+  container.appendChild(price);
+
+  card.appendChild(container);
+  document.getElementById("products").appendChild(card);
+}
+
+//parameter passed from button (Parameter same as category)
+function filterProduct(value) {
+  //Button class code
+  let buttons = document.querySelectorAll(".button-value");
+  buttons.forEach((button) => {
+    //check if value equals innerText
+    if (value.toUpperCase() == button.innerText.toUpperCase()) {
+      button.classList.add("active");
+    } else {
+      button.classList.remove("active");
+    }
+  });
+
+  //select all cards
+  let elements = document.querySelectorAll(".card");
+  //loop through all cards
+  elements.forEach((element) => {
+    //display all cards on 'all' button click
+    if (value == "all") {
+      element.classList.remove("hide");
+    } else {
+      //Check if element contains category class
+      if (element.classList.contains(value)) {
+        //display element based on category
+        element.classList.remove("hide");
+      } else {
+        //hide other elements
+        element.classList.add("hide");
+      }
+    }
+  });
+}
+
+//Search button click
+document.getElementById("search").addEventListener("click", () => {
+  //initializations
+  let searchInput = document.getElementById("search-input").value;
+  let elements = document.querySelectorAll(".product-name");
+  let cards = document.querySelectorAll(".card");
+
+  //loop through all elements
+  elements.forEach((element, index) => {
+    //check if text includes the search value
+    if (element.innerText.includes(searchInput.toUpperCase())) {
+      //display matching card
+      cards[index].classList.remove("hide");
+    } else {
+      //hide others
+      cards[index].classList.add("hide");
+    }
+  });
+});
+
